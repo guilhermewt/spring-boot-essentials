f58639d681647f0c6742e779cec6a45106b28890
welliston <welliston10@gmail.com>
2022-05-31 19:31:45 -0300

    metodo post, delete, put

--- spring-boot-essentials/src/main/java/academy/devdojo/springboot2/service/animeService.java
+++ spring-boot-essentials/src/main/java/academy/devdojo/springboot2/service/animeService.java
@@ -1,6 +1,8 @@
 package academy.devdojo.springboot2.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -13,18 +15,38 @@ public class animeService {
 	
 	//private final AnimeRepository animeRepository;
 	
-	private List<Anime> anime  = List.of(new Anime(1l,"DBZ"), new Anime(2l,"BERSEKd"));
+	private static List<Anime> animes;
+	
+	static {
+		animes = new ArrayList<>(List.of(new Anime(1l,"DBZ"), new Anime(2l,"BERSEKd")));
+	}
+
 	
 		
 	public List<Anime> listAll(){
-		return anime;
+		return animes;
 	}
 	
 	public Anime findById(long id){
-		return anime.stream()
+		return animes.stream()
 				.filter(anime -> anime.getId().equals(id))
 				.findFirst()
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "anime not found"));
+	}
+	
+	public Anime save(Anime anime) {
+		anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+		animes.add(anime);
+		return anime;
+	}
+	
+	public void delete(long id){
+		animes.remove(findById(id));
+	}
+	
+	public void replace(Anime anime){
+		animes.remove(findById(anime.getId()));
+		animes.add(anime);
 	}
 	
 	
